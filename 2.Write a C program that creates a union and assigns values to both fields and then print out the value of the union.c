22e41a7764b7bd44219bfbd7323af6adcd60709e
vanthiyathevan7 <129197814+vanthiyathevan7@users.noreply.github.com>
2023-03-31 12:34:45 +0530

    Add files via upload

---
+++ 2.Write a C program that creates a union and assigns values to both fields and then print out the value of the union.c
@@ -0,0 +1,31 @@
+# include<stdio.h>
+# include<conio.h>
+struct  employee
+{
+char name[50] ;
+int empId ;
+int age ;
+float salary ;
+} ;
+
+int  main( )
+{
+struct employee emp ;
+
+printf("\n Enter employee details\n") ;
+printf("\n Name : ") ;
+gets(emp.name) ;
+printf("\n ID : ") ;
+scanf("%d",&emp.empId ) ;
+printf("\n Age : ") ;
+scanf("%d",&emp.age ) ;
+printf("\n Salary : ") ;
+scanf("%f",&emp.salary ) ;
+
+printf("\n Entered employee detail are !" ) ;
+printf("\n Name: %s" ,emp.name ) ;
+printf("\n Id: %d" ,emp.empId ) ;
+printf("\n Id: %d" ,emp.age ) ;
+printf("\n Salary: %f\n",emp.salary ) ;
+return 0 ;
+}
